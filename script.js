f8bd3f629e777fc0cea0bdbccd4c621dfa9579ee
tesserxt <142136334+Tesserxt@users.noreply.github.com>
2025-03-22 16:28:24 +0530

    refactored display() to work on last object in myLibrary
    added form handling, validation to add new books

--- script.js
+++ script.js
@@ -29,62 +29,89 @@ function Book(title, author, nPages, isRead) {
     this.id = crypto.randomUUID()
 }
 
+Book.prototype.toggleRead = function (e) {
+    if (e.target.id === 'read') {
+        let status = e.target.textContent
+        this.isRead = (status === 'Read') ? e.target.textContent = 'Unread' : e.target.textContent = 'Read';
+    }
+}
+
 function addBookToLibrary(title, author, nPages, isRead) {
     const book = new Book(title, author, nPages, isRead)
     myLibrary.push(book)
 }
 
-function display(myLibrary) {
+function display() {
 
     const container = document.querySelector('.container');
-
-    myLibrary.map((obj) => {
-        container.innerHTML += cardHTML;
-
-        //sets unique Book.id to each card
-        const cards = document.querySelectorAll('.card');
-        cards.forEach((card) => {
-            card.setAttribute('id', obj.id)
-        });
-        
-        const title = document.querySelectorAll('.title');
-        const author = document.querySelectorAll('.author');
-        const nPages = document.querySelectorAll('.npages');
+    let obj = myLibrary[myLibrary.length - 1]
 
 
-        title[title.length - 1].textContent = obj.title;
-        author[author.length - 1].textContent = obj.author;
-        nPages[nPages.length - 1].textContent = obj.nPages;
+    container.innerHTML += cardHTML;
 
-        const buttons = document.querySelectorAll("button");
-        buttons.forEach((button) => {
-            button.addEventListener("click", (e) => {
-                
-                if (e.target.id === 'read') {
-                    let status = e.target.textContent
-                    obj.isRead = (status === 'Read') ? e.target.textContent = 'Unread' : e.target.textContent = 'Read';
-                    // button.style.backgroundColor = 'green'
-                    console.log(obj.isRead);
-                }
-
-                //removing book card from DOM and myLibrary
-                else if (e.target.id === 'remove') {
-                    const clicked_card = button.closest('.card');
-                    myLibrary = myLibrary.filter((obj) => (!obj.id === clicked_card.id));
-                    clicked_card.remove();
-
-                }
-            });
-        });
-
-
+    //sets unique Book.id to each card
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card) => {
+        card.setAttribute('id', obj.id)
     });
+
+    const title = document.querySelectorAll('.title');
+    const author = document.querySelectorAll('.author');
+    const nPages = document.querySelectorAll('.npages');
+    const isRead = document.querySelectorAll('#read');
+
+
+    title[title.length - 1].textContent = obj.title;
+    author[author.length - 1].textContent = obj.author;
+    nPages[nPages.length - 1].textContent = obj.nPages;
+    isRead[isRead.length - 1].textContent = obj.isRead;
+
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", (e) => {
+
+            obj.toggleRead(e);
+            console.log(obj.isRead)
+
+            //removing book card from DOM and myLibrary
+            if (e.target.id === 'remove') {
+                const clicked_card = button.closest('.card');
+                myLibrary = myLibrary.filter((obj) => (!obj.id === clicked_card.id));
+                clicked_card.remove();
+
+            }
+        });
+    });
+
+
 }
 
-addBookToLibrary('Lost in maya', 'god', '1000', false)
-addBookToLibrary('Lost in m', 'god', '1000', false)
+
+function validateForm(data) {
+    return data.title.length > 0 && data.author.length > 0 && data.pages.length > 0
+}
 
 
-display(myLibrary);
-console.log('radha')
+
+const form = document.querySelector('form');
+function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const data = {};
+    for (let keyValue of formData.entries()) {
+        data[keyValue[0]] = keyValue[1];
+    }
+    if (validateForm(data)) {
+        addBookToLibrary(data.title, data.author, data.pages, data.isRead);
+        display();
+    }
+    else{
+        alert('Add the book details first.')
+    }
+
+
+
+}
+form.addEventListener('submit', handleSubmit);
 
